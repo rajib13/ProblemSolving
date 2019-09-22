e25637a6ff90b6b9409ddffe7a4813a01e4b6f92
rajib13 <rajib.das@uleth.ca>
2019-09-22 16:20:34 -0400

    Finding the apples number

---
+++ leetcode/1196-How-Many-apples-can-you-put-into-the-basket.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxNumberOfApples(vector<int>& arr) {
+        
+        sort(arr.begin(), arr.end()); // first sort the array
+        int sum = 0; int i = 0;
+        for( i=0; i<arr.size(); i++){ 
+		sum += arr[i]; 
+           	 if(sum > 5000) return i; 
+           	 else if(sum == 5000) return i+1; 
+        }
+        return i;
+    }
+};
