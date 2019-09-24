93dbc64e8ab44521987484555b12e6e0c3dfe5e0
rajib13 <rajib.das@uleth.ca>
2019-09-24 00:22:24 -0400

    Remove duplicates from sorted array in O(1) time.

---
+++ leetcode/26-Remove-Duplicates-from-Sorted-Array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        /*Approach 2: one liner*/
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+        return nums.size();
+    }
+};
+
+
+/* Approach 1: brute force. O(n) time and O(1) space. 
+        if(nums.size() == 0) return 0;
+        
+        int count = 1; 
+        for(int i=1; i< nums.size(); i++){
+            if(nums[i-1] != nums[i]){
+                nums[count++] = nums[i];
+            }
+        }
+        
+     return count; 
+
+*/
