93ca3a49f7c9caf6a6f33274a17a7f4e53383af3
rajib13 <rajib.das@uleth.ca>
2019-10-13 01:39:41 -0400

    Code updated.

--- leetcode/1221-Split-A-String-in-Balanced-Strings.cpp
+++ leetcode/1221-Split-A-String-in-Balanced-Strings.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int balancedStringSplit(string s) {
         int count = 0;
-        int R = 0;
+        int sum = 0;
         for(char ch : s){
-            if(ch == 'R') R++;
-            else R--;
-            if(R == 0) count++;
+            if(ch == 'R') sum++;
+            else sum--;
+            if(sum == 0) count++;
         }
         return count; 
     }
