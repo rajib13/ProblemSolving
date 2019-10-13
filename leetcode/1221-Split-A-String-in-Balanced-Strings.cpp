d74a625ec61a8ee08be6bffe3e4b85709030bf07
rajib13 <rajib.das@uleth.ca>
2019-10-13 01:49:07 -0400

    Clean and most updated code.

--- leetcode/1221-Split-A-String-in-Balanced-Strings.cpp
+++ leetcode/1221-Split-A-String-in-Balanced-Strings.cpp
@@ -4,8 +4,7 @@ public:
         int count = 0;
         int sum = 0;
         for(char ch : s){
-            if(ch == 'R') sum++;
-            else sum--;
+            sum += ch == 'R' ? 1 : -1;
             if(sum == 0) count++;
         }
         return count; 
