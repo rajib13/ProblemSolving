9550859117b621faac482310bb811166e0ffc4b0
rajib13 <rajib.das@uleth.ca>
2019-10-14 18:00:20 -0400

    Code updated with the loop termination.

--- problems/341-reverse-string.cpp
+++ problems/341-reverse-string.cpp
@@ -3,7 +3,7 @@ public:
     void reverseString(vector<char>& s) {
         int length = s.size(); // determine the size of the given string.
         if(length > 0){ // crucial because of the empty string.
-            for(int i = 0; i <= length/2; i++){ // As we are going to use two pointers from the beginning and the end, it is sufficient to check untill half of the string length.
+            for(int i = 0; i < length/2; i++){ // As we are going to use two pointers from the beginning and the end, it is sufficient to check untill half of the string length.
                 swap(s[i], s[length-i-1]); // length-- is not work, becuase in for loop we determine the limit i every time.  
             }
         }
