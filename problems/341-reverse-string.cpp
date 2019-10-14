54958b27bbdbe0828c8272541f52f3313e09273e
rajib13 <rajib.das@uleth.ca>
2019-10-14 17:51:12 -0400

    Reversing a string in place.

---
+++ problems/341-reverse-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int length = s.size();
+        if(length > 0){
+            for(int i = 0; i <= (length - 1)/2; i++){
+                swap(s[i], s[length-i-1]);
+            }
+        }
+
+    }
+};
