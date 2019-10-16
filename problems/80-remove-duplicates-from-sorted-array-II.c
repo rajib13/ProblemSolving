e8d8a4e0e51624ef37e55b53044e83995f165959
rajib13 <rajib.das@uleth.ca>
2019-10-16 00:21:13 -0400

    Added second approach to the code.

--- problems/80-remove-duplicates-from-sorted-array-II.c
+++ problems/80-remove-duplicates-from-sorted-array-II.c
@@ -1,3 +1,17 @@
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int count = 0; 
+        for(int num : nums){
+            if(count < 2 || num > nums[count-2]) nums[count++] = num;
+        }
+        return count;
+    }
+};
+
+/*
+Approach 2: 
 int removeDuplicates(int* nums, int numsSize){
     int prev = -100, count = 0, occur = 0;
     for(int i = 0; i < numsSize; i++){
@@ -9,5 +23,5 @@ int removeDuplicates(int* nums, int numsSize){
            
     }
 return count; 
-}
+}*/
 
