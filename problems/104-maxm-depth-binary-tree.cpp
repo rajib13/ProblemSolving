41dbedfd0b2f92bd16f821ffbe4ec990e3d6161b
rajib13 <rajib.das@uleth.ca>
2019-10-20 02:29:45 -0400

    Finding the maximum depth of a binary tree.

---
+++ problems/104-maxm-depth-binary-tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public: 
+    int maxDepth(TreeNode* root) {
+     return root == NULL? 0 : max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
+
+/*
+Note:   if we were asked to find the max depth by counting edges then in base case
+        we would return -1. That means when there is no node we should return -1
+        because a node without child has no edge, it should return 0 and so if there is no 
+        root then -1. 
+        
+        See Hackerrank problem: https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=trees
+
+*/
