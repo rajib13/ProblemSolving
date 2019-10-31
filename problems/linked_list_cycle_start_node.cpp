78453b8a05d21356f3b27d5ccab7f247f5cf2ad3
rajib13 <rajib.das@uleth.ca>
2019-10-31 02:05:02 -0400

    Find the start node of cycle in a linked list.

---
+++ problems/linked_list_cycle_start_node.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        bool cycle = false;
+        while(fast and fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(slow == fast) {
+                cycle = true;
+                break;
+            }
+            
+        }
+        
+        if(!cycle) return nullptr; // if pos = -1, case. 
+        
+        /*Let,
+        x = the distance from head node to start of the cycle.
+        y = from start of the cycle to meeting point of slow and fast pointers.
+        z = from meeting point to the start node of the cycle in other way around.
+        
+        So, the slow pointer covered x + y and fast pointer covered x + 2y + z (because its already walked through the cycle) and fast pointer covered actually twice the distance of the slow pointer.
+        
+        Mathematically, 2(x + y) = x + 2y + z;
+        Therefore, x = z; That means if we again start the slow pointer from the beginning and fast pointer from the meeting point, then they will meet again on the node where the cycle begins.
+        */
+        
+        slow = head;
+        
+        while(slow != fast){
+            fast = fast->next;
+            slow = slow->next;
+            if(slow == fast) return slow;
+        }
+        return slow;
+        
+    }
+};
+
+/*
+Test Cases:
+
+[3,2,0,-4]
+1
+[1,2]
+0
+[1]
+-1
+[1]
+0
+
+*/
