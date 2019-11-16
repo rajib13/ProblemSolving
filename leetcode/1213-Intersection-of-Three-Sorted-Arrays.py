414163d9cfc48bb367824705e64c896db0fbdbf9
rajib13 <rcdas09@gmail.com>
2019-11-16 11:56:45 -0500

    Added 1256 encode number.

--- leetcode/1213-Intersection-of-Three-Sorted-Arrays.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
-        return sorted(set(arr1).intersection(set(arr2), set(arr3))) // sorted is crucial because set will give us a unsorted list
-        
--- leetcode/342-Power-Of-Four.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    bool isPowerOfFour(int n) {
-        return fmod((log10(n)/log10(4)), 1) == 0;
-        
-    }
-};
