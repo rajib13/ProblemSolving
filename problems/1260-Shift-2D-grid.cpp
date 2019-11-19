c11b340f468b0d9a3a2cfe03e93d474e64024ff2
rajib13 <rcdas09@gmail.com>
2019-11-18 23:31:21 -0500

    Shift a 2D matrix by k times.

---
+++ problems/1260-Shift-2D-grid.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int turns) {
+        int row = grid.size()  - 1; 
+        int col = grid[0].size() - 1;
+        
+        for(int k = 0; k < turns; k++){
+            for(int i = row; i > 0; i--){
+                for(int j = col; j > 0;  j--){
+                    swap(grid[i][j], grid[i][j-1]);
+                }
+                swap(grid[i-1][col], grid[i][0]);
+            
+            }
+            for(int i = col; i > 0; i--)
+                swap(grid[0][i], grid[0][i-1]);
+        
+        }
+        
+        return grid;
+    }
+};
