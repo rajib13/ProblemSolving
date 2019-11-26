3184c299a61e392a00066cc6f0e2d1ae8d243292
rajib13 <rcdas09@gmail.com>
2019-11-25 23:01:01 -0500

    Meerging Intervals

---
+++ problems/56-merge-intervals.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> overlap;
+    
+        if(intervals.size() == 0) return overlap;
+        
+        sort(intervals.begin(), intervals.end());
+        
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        for(int i = 1; i < intervals.size(); i++){
+            
+            if(end< intervals[i][0]){
+                overlap.push_back({start, end});
+                    start = intervals[i][0];
+            }
+            end = max(end, intervals[i][1]);
+        }
+        overlap.push_back({start, end});
+        return overlap;
+    }
+};
