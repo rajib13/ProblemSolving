f98d7ffdf483b26b621dd6e4a7801a3cc4a2a6e1
rajib13 <rcdas09@gmail.com>
2019-12-19 02:10:26 -0500

    Finding the trailing zeroes of factorial of a number

---
+++ problems/Factorial-trailing-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int trailingZeroes(int num) {
+        
+        int count = 0;
+        /*Idea: Trailing zeroes can only possible with 2 and 5. So it is enough to find out the multiples of 5 because 2 will be more than 5 and as we need both 
+        of them, we can just take minimum number of them. 15 = 3  * 5 so there are only one zero, but 25 = 5 * 5, that means there are two zeroes.*/
+        
+        long i = 5; 
+        while(num/i){
+            count += num/i;
+            i *= 5; 
+        }
+        return count;
+    }
+};
