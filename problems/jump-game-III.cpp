89bcff4c04e8212b89eb065d92c63ee324977148
rajib13 <rcdas09@gmail.com>
2019-12-28 23:22:26 -0500

    Code updated.

--- problems/jump-game-III.cpp
+++ problems/jump-game-III.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    set<int> seen; // To track the visited node so that we do not fall in loop or TLE. 
+    set<int> seen; // To track the visited node so that we do not fall in loop or get TLE. 
     
     bool canReach(vector<int>& arr, int start) {
         if(start >= arr.size() or start < 0 or seen.find(start) != seen.end()) return false; // Boundary cases
         seen.insert(start); // populate the seen container
         
         if(arr[start] == 0) return true; // success case
-        eturn canReach(arr, start + arr[start]) or canReach(arr, start - arr[start]); // recursive call for two scenarios.
+        return canReach(arr, start + arr[start]) or canReach(arr, start - arr[start]); // recursive call for two scenarios.
     }
         
 };
