13f462237bb0a3da0204441a47993aa3b3e2c3c8
rajib13 <rcdas09@gmail.com>
2019-12-29 21:37:40 -0500

    Sort red, white, blue objects in O(n) time and one pass.

---
+++ problems/sort-colors.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        int redPointer = 0; 
+        int bluePointer = nums.size() - 1; 
+        
+        
+        for(int i = 0; i <= bluePointer; i++){
+            if(nums[i] == 0){
+                swap(nums[redPointer++], nums[i]);
+            }
+            else if(nums[i] == 2){
+                swap(nums[bluePointer--], nums[i--]); // Be careful about i--. Think about it, the answer is nice. See the note. 
+                
+            }
+        }
+        
+    }
+};
+
+/*
+
+[2,0,2,1,1,0]
+[0,0,1,1,2,2]
+[0,1,2]
+[2,0,1]
+[0]
+[1]
+[2]
+[0,0,0,0,0]
+[2,2,2,2,0,0,0]
+[1,1,1,1,1,0,0,0,0,2,2,2,2]
+[1,2,0]
+[2,1,2]
+
+*/
