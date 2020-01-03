0aa5599f3088af292f70af80857f562fab05067c
rajib13 <rcdas09@gmail.com>
2020-01-03 00:25:43 -0500

    Deepest Leaves Sum of a binary tree.

---
+++ problems/deepest-leaves-sum.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        if(root == nullptr) return 0;
+        
+        int levelSum = 0;
+        
+        queue<TreeNode*> nodes;
+        nodes.push(root);
+        while(!nodes.empty()){
+            levelSum = 0; 
+            int qSize = nodes.size();
+            for(int i = 0 ; i < qSize; i++){
+                root = nodes.front(); nodes.pop();
+                levelSum += root->val;
+                if(root->left) nodes.push(root->left);
+                if(root->right) nodes.push(root->right);
+            }
+            
+        }
+        return levelSum;
+        
+    }
+};
