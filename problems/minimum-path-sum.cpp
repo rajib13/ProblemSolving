45c0cf965052d96b5522fb573a66f8677fb5aac4
rajib13 <rcdas09@gmail.com>
2020-01-07 00:23:59 -0500

    Finding the minimum path sum.

---
+++ problems/minimum-path-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        
+        int row = grid.size();
+        int col = grid[0].size();
+        
+        for(int i = 0; i < row; ++i){
+            for(int j = 0; j < col; ++j){
+                if(i == 0 and j > 0) grid[i][j] += grid[i][j-1]; // In frist row, jsut add the columns up. 
+                else if(j == 0 and i > 0) grid[i][j] += grid[i-1][j]; // In first column, add the rows up. 
+                else if(i > 0 and j > 0) grid[i][j] += min(grid[i-1][j], grid[i][j-1]); // in general case. 
+            }
+        }
+        return grid[row-1][col-1];
+    }
+};
