a22e95a4b1e2c876e504a089859092b8a8843930
rajib13 <rcdas09@gmail.com>
2020-01-10 00:53:30 -0500

    Count Unique paths in a grid with obstacles.

---
+++ problems/unique-paths-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        
+        int row = grid.size();
+        int col = grid[0].size();
+        if(grid[0][0] == 1) return 0; // if start point is an obstacle. 
+        
+         
+        vector<vector<long>> dp(row, vector<long> (col, 0)); // Need this extra vector becausee one of the test cases has int overflow.
+        dp[0][0] = 1; // To check the test case [[0]] type. 
+        
+        // Fill the  first column with 1 until approaching an obstacle and after that with 0. 
+        int value = 1;
+        for(int r = 1; r < row; r++){
+            if(grid[r][0] == 1) value = 0; 
+            dp[r][0] = value;
+        }
+        // Similarly, Fill the first row.
+        value = 1;
+        for(int c = 1; c < col; c++){
+            if(grid[0][c] == 1) value = 0; 
+            dp[0][c] = value;
+        }
+        
+        for(int r = 1; r < row; r++){
+            for(int c = 1; c < col; c++){
+                dp[r][c] = grid[r][c] == 1 ? 0 : dp[r-1][c] + dp[r][c-1]; // DP Logic
+            }
+        }
+        return dp[row-1][col-1];
+    }
+};
