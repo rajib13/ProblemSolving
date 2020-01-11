002607beb00e4f230cd1c420ac6b5c2ebcb4fec5
rajib13 <rcdas09@gmail.com>
2020-01-11 14:24:40 -0500

    Code updated.

--- problems/unique-paths.cpp
+++ problems/unique-paths.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int uniquePaths(int row, int col) {
         vector<int> dp(col, 1);
-        // Approach 2: We don't need 2D vector. Just add the previous column value to the current one is enough to get the solution. 
+        // Approach 2: Just add the previous column value to the current one is enough to get the solution. 
         for(int r = 1; r < row; r++){
             for(int c = 1; c < col; c++){
                 dp[c] += dp[c-1];
@@ -17,7 +17,26 @@ public:
 
 
 /*
+class Solution {
+public:
+    int uniquePaths(int row, int col) {
+        Approach 2:  We don't need 2D vector. We only two rows, to make the second row from the first one. 
+        vector<vector<int>> dp(2, vector<int> (col, 1));
+        
+        for(int r = 1; r < row; r++){
+            for(int c = 1; c < col; c++){
+                dp[1][c] = dp[0][c] + dp[1][c-1];
+            }
+            for(int c = 1; c < col; c++)
+                dp[0][c] = dp[1][c];
+        }
+        
+        return dp[1][col-1];
+        
+    }
+};
 
+/*
 class Solution {
 public:
     int uniquePaths(int row, int col) {
