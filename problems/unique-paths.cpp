3a85dc5f2e41dc0cc1f45e063c4c461318e67916
rajib13 <rcdas09@gmail.com>
2020-02-06 23:59:33 -0500

    Comment updated

--- problems/unique-paths.cpp
+++ problems/unique-paths.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int uniquePaths(int row, int col) {
         vector<int> dp(col, 1);
-        // Approach 2: Just add the previous column value to the current one is enough to get the solution. 
+        // Approach 2: Just add the previous column value to the current one is enough to get the solution
         for(int r = 1; r < row; r++){
             for(int c = 1; c < col; c++){
                 dp[c] += dp[c-1];
