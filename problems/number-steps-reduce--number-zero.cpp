b25a26591d9aa883c6cf22227797f196d5e29882
rajib13 <rcdas09@gmail.com>
2020-02-09 16:01:26 -0500

    Comment updated.

--- problems/number-steps-reduce--number-zero.cpp
+++ problems/number-steps-reduce--number-zero.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int numberOfSteps (int num) {
+	/*Approach 1: Straight forward solution.*/
         int count = 0 ;
         while(num){
             if(num % 2 == 0) num /= 2;
