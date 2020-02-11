4a76b8e844d34521e3f22b38bf6fe1a91db3f5bf
rajib13 <rcdas09@gmail.com>
2020-02-10 21:34:27 -0500

    Comment updated.

--- problems/angle-between-hands-of-clock.cpp
+++ problems/angle-between-hands-of-clock.cpp
@@ -3,13 +3,12 @@ public:
     double angleClock(int hr, int m) {
         // Idea: For every hour the covered angle is 30 and for every minute that is 6. 
         
-        
-        double ret;
+        double ret; 
 	// Angle covered by hour (hour hand plus for the minutes hand)
         double hrAngle = (double) (hr % 12) * 30 + (double) m/60 * 30;
         double minAngle = m * 6; 
         
-        ret = abs (minAngle - hrAngle);
+        ret = abs(minAngle - hrAngle);
         
         return min(ret, 360-ret);
     }
