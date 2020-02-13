56142c87f21a74562d4be6ea0ffa389a80808c30
rajib13 <rcdas09@gmail.com>
2020-02-12 22:12:33 -0500

    Comment updated.

--- problems/find-smallest-letter-greater-than-target.cpp
+++ problems/find-smallest-letter-greater-than-target.cpp
@@ -7,7 +7,7 @@ public:
         
         while(low <= high){
             int mid = low + (high - low)/2;
-            if(letters[mid]  <= target) low = mid + 1;
+            if(letters[mid]  <= target) low = mid + 1; // If we find the target we are not sure that is the greater (e.g. e, e, e, n). So we need to move right side further.
             else high = mid - 1; 
         }
         return letters[low%length]; 
