458de4206e8cd9af8547a9c8e214ef9abcf428c9
rajib13 <rcdas09@gmail.com>
2020-02-15 23:50:19 -0500

    Comment updated.

--- problems/count-negative-num-in-a-sorted-matrix.cpp
+++ problems/count-negative-num-in-a-sorted-matrix.cpp
@@ -5,8 +5,9 @@ public:
         int count = 0;
         int row = grid.size() - 1;
         int col = grid[0].size() - 1;
-	/*Approach: As we are given sorted matrix (row and column wise), we can negative numbers from bottom right corner. 
-Once we find out a non-negative number we immediately stop scanning the matrix, and return count.*/
+	/*Approach: As we are given sorted matrix (row and column wise), that means it is more optimized
+ 	if we start scanning from the bottom right corner of the matrix. Once we find out a non-negative number
+ 	we immediately stop scanning the matrix, and return count.*/
         
         for(int i = row; i >= 0; i--){
             for(int j = col; j >= 0; j--){
