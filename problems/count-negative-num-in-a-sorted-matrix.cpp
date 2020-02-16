9076ae446f3a7947546194ccaf0212746620b5ca
rajib13 <rcdas09@gmail.com>
2020-02-15 23:47:22 -0500

    Find total numbers of negative numbers in a sorted matrix.

--- problems/count-negative-num-in-a-sorted-matrix.cpp
+++ problems/count-negative-num-in-a-sorted-matrix.cpp
@@ -5,6 +5,8 @@ public:
         int count = 0;
         int row = grid.size() - 1;
         int col = grid[0].size() - 1;
+	/*Approach: As we are given sorted matrix (row and column wise), we can negative numbers from bottom right corner. 
+Once we find out a non-negative number we immediately stop scanning the matrix, and return count.*/
         
         for(int i = row; i >= 0; i--){
             for(int j = col; j >= 0; j--){
