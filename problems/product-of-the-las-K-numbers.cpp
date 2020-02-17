e9f6abad846693aba000be3e0d1ceb0b8d768c29
rajib13 <rcdas09@gmail.com>
2020-02-16 23:42:09 -0500

    Find the product of the last k numbers that were inserted.

--- problems/product-of-the-las-K-numbers.cpp
+++ problems/product-of-the-las-K-numbers.cpp
@@ -1,8 +1,9 @@
 class ProductOfNumbers {
 public:
+	/*Approach: Prefix product like prefix sum and then in constant time get the solution.*/
     vector<int> vec;
     ProductOfNumbers() {
-        vec.push_back(1);
+        vec.push_back(1); // Initialy the vector should contain 1 as we are going to implement prefix product algorithm.
     }
     
     void add(int num) {
@@ -15,7 +16,7 @@ public:
     
     int getProduct(int k) {
         int sz = vec.size();
-        return sz > k ? vec.back()/vec[(sz - 1) - k] : 0;
+        return sz > k ? vec.back()/vec[(sz - 1) - k] : 0; // if in last k numbers there is/are a zero(s) then we simply return 0
     }
 };
 
