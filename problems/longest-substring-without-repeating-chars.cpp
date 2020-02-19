54a4dd2eef1cd23797735a01137a1425c2531165
rajib13 <rcdas09@gmail.com>
2020-02-18 23:58:39 -0500

    Find the longest substring in a string with unique characters.

---
+++ problems/longest-substring-without-repeating-chars.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        /*Approach: Two pointers search. second pointer will always increase by 1 and the first pointer will 
+        increase by the position we have seen the char before. As array starts with 0, we need to add extra one.*/
+        if(s.size() < 2) return s.size();
+        
+        unordered_map<char, int> chars;
+        
+        int maxx = INT_MIN;
+        int i = 0; // first pointere
+        for(int j = 0; j < s.size(); j++){ // j = 2nd pointer
+            if(chars.count(s[j])){
+                i = max(i, chars[s[j]] + 1);
+            }
+            maxx = max(maxx, j - i + 1);
+            cout << maxx << " " << i << endl;
+            chars[s[j]] = j;
+        }
+        
+        return maxx;
+        
+    }
+};
+
+/*
+"abcabcbb"
+"pwwkew"
+"bbbbb"
+"        "
+" "
+"dvdf"
+"abccbabacd"
+*/
