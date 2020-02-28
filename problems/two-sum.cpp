2faa63b05113578ba883662d8b86b042d044e81f
rajib13 <rcdas09@gmail.com>
2020-02-27 23:59:21 -0500

    Code Updated.

--- problems/two-sum.cpp
+++ problems/two-sum.cpp
@@ -5,7 +5,7 @@ public:
         map<int, int> myMap; //map to store pairs
         
         for(int i=0;i<nums.size();i++){
-            int subtract = target-nums[i]; //get the difference between target and the current nums[i] value
+            int subtract = target - nums[i]; //get the difference between target and the current nums[i] value
             if(myMap.count(subtract)){ // check the difference is in the map or not
                 
                 return {myMap.at(subtract), i}; // find out the index of the difference value
