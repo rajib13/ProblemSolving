97cc5de037b3bb3e245a70b185473e30d8b3f6f7
rajib13 <rcdas09@gmail.com>
2020-03-06 23:50:24 -0500

    Comment updated.

--- problems/roman-to-int.cpp
+++ problems/roman-to-int.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int romanToInt(string s) {
         
-        unordered_map<char, int> exchange; // To map the map and integer number. 
+        unordered_map<char, int> exchange; // To map the roman and integer number. 
         exchange['I'] = 1;
         exchange['V'] = 5;
         exchange['X'] = 10;
@@ -12,7 +12,7 @@ public:
         exchange['M'] = 1000;
         
         int ret = 0;
-        for(int i = 0 ; i  < s.size(); i++){
+        for(int i = 0 ; i  < s.size() - 1; i++){
             if(i+1 < s.size() and exchange[s[i]] < exchange[s[i+1]]) ret -= exchange[s[i]]; // Only if the next number is greater than the current number then we need to deduct it from the sum.
             else ret += exchange[s[i]];
     
