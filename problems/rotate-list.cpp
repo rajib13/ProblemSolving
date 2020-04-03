8a9fc15066e084557076848487b43711a2607831
rajib13 <rcdas09@gmail.com>
2020-04-03 00:28:58 -0400

    Rotate a linked list by k times to the right.

--- problems/rotate-list.cpp
+++ problems/rotate-list.cpp
@@ -9,6 +9,7 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
+        if(head == nullptr) return head;
         ListNode* root = head;
         int nodeCount = 0;
         while(root){
@@ -16,17 +17,41 @@ public:
             nodeCount++;
         }
         k %= nodeCount;
+        if(nodeCount == k || k == 0) return head;
         
         root = head;
+        ListNode* prevNode = nullptr;
         
-       for(int i = 0; i <= k; i++){
+       for(int i = 0; i < nodeCount - k; i++){
+            prevNode = root;
             root = root->next;
         }
-        ListNode* ret = root->next;
-        ListNode* temp = ret;
+        prevNode->next = nullptr;
+        ListNode* temp = root;
         while(temp->next) temp = temp->next;
         temp->next = head;
+        
     
-        return ret;
+        return root;
     }
 };
+
+/*
+[1,2,3,4,5]
+1
+[1,2,3,4,5]
+2
+[1,2,3,4,5]
+3
+[1,2,3,4,5]
+4
+[1,2,3,4,5]
+5
+[1,2,3,4,5]
+6
+[1,2,3,4,5]
+7
+[1,2,3,4,5]
+192
+
+*/
