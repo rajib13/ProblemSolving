f291925512c3a357c58c00aadaaff1b3e78075f6
rajib13 <rcdas09@gmail.com>
2020-04-05 23:59:39 -0400

    updated.

--- problems/remove-nth-node-from-end-of-list.cpp
+++ problems/remove-nth-node-from-end-of-list.cpp
@@ -29,6 +29,7 @@ public:
 /*Complexity Analysis: O(n) time as we need to traverse all link nodes at least once and O(1) space, as we did not use any extra space. */
 
 /*
+Test Cases:
 [1,2,3,4,5]
 1
 [1,2,3,4,5]
