e3cf569456524a9b8dbe9c8c5d32c6944ba92bce
rajib13 <rcdas09@gmail.com>
2020-04-08 22:12:42 -0400

    Add a range to an Interval range.

---
+++ problems/interval-range.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& ranges, vector<int>& newInterval) {
+        
+        int start = newInterval[0];
+        int end = newInterval[1];
+        vector<vector<int>> result;
+        int i = 0;
+        int sz = ranges.size();
+        while(i < sz and ranges[i][1] < start) result.push_back(ranges[i++]);
+        while(i < sz and ranges[i][0] <= end){
+            start = min(ranges[i][0], start);
+            end = max(ranges[i][1], end);
+            i++;
+        }
+        result.push_back({start, end});
+        while(i < sz and ranges[i][0] > end) result.push_back(ranges[i++]);
+        
+        return result;
+    }
+};
+
+/*
+Complexity analysis:
+Time : O(n), as we need to scan all range in the ranges.
+Space: O(1), as we do use any extra space other than return result vector.
+*/
