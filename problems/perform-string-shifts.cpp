d1435675f83759d3da893d0a69add068f95a8f1a
rajib13 <rcdas09@gmail.com>
2020-04-14 23:43:34 -0400

    Perform string left and right shifts.

---
+++ problems/perform-string-shifts.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string stringShift(string s, vector<vector<int>>& shifts) {
+        
+        int count = 0; 
+        for(vector<int> shift : shifts){
+            count += shift[0] ? -shift[1] : shift[1];
+        }
+        count  %= int (s.size());
+        if(count > 0){
+            rotate(s.begin(), next(s.begin(), count), s.end());
+        }
+        else {
+            rotate(s.rbegin(), next(s.rbegin(), -count), s.rend());
+        }
+        
+        return s;
+    }
+};
+
+/*
+Complexity Analysis:
+Time: O(n), where n is the size of shifts.
+Space: O(1), as we do not use any extra space.
+*/
