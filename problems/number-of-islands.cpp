5296ffae6dfe85b65506dd12837f4dcd5782d47c
rajib13 <rcdas09@gmail.com>
2020-04-18 01:44:37 -0400

    Find the number of islands.

---
+++ problems/number-of-islands.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid, int i, int j){
+        if(i >= grid.size() or i < 0 or j >= grid[0].size() or j < 0 or grid[i][j] != '1') return;
+        grid[i][j] = '0';
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i, j-1);
+        
+    }
+    int numIslands(vector<vector<char>>& grid) {
+      
+        int count = 0; 
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[0].size(); j++){
+                if(grid[i][j] == '1'){
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+};
