ec33c81809f50c3fdb35bdcbcb73a9a41b3b7d7a
rajib13 <rcdas09@gmail.com>
2020-04-22 23:35:42 -0400

    Determine whether two binary trees are same or not.

---
+++ problems/same-tree.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ 
+ */
+ class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        /*Approach 2: recursion. O(n) and O(log n) space - DFS.*/
+        if(p == NULL or q == NULL ) return p==q;
+        
+        //if(p and q == NULL || q and p == NULL || p->val != q->val) return false;
+        
+        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        
+    }
+};
+ 
+ 
+
+/*Approach 1 : O(n) runtime and O(log n) space - BFS level order traversal.
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+        if(p and q == NULL || q and p == NULL) return false;
+        
+        stack<TreeNode*> nodesP;
+        stack<TreeNode*> nodesQ;
+        
+        while(!nodesP.empty() or !nodesQ.empty() or p != NULL){
+            while( p != NULL or q!= NULL){
+                
+                if(q == NULL or p == NULL or p->val != q->val) return false;
+                nodesQ.push(q);
+                nodesP.push(p);
+                p = p->left;
+                q = q->left;
+            }
+            p = nodesP.top(); nodesP.pop();
+            q = nodesQ.top(); nodesQ.pop();
+            
+            if(p->val != q->val) return false;
+            p = p->right;
+            q = q->right;
+        }
+        return true;
+        
+    }
+};
