86f8f7bd6e05142c08d092a863f51a9bce17cf9f
rajib13 <rcdas09@gmail.com>
2020-04-25 22:06:10 -0400

    Make group of words if two words are anagrams of each other.

---
+++ group-anagrams.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map<string, vector<string>> mp;
+        for(string word : strs){
+            string temp = word;
+            sort(word.begin(), word.end());
+            mp[word].push_back(temp);
+        }
+        vector<vector<string>> ret;
+        
+        for(auto entry : mp){
+            ret.push_back(entry.second);
+        }
+        return ret;
+    }
+};
+
+/*
+Complexity analysis:
+Time: O(m*nlogn), where m is the total number of words, n is the length of longest word.
+Space: O(m), as we need to store them in a hashmap.*/
