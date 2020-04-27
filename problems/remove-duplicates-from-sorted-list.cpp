57cd38aef838308bdda589280fc757f998fd84a6
rajib13 <rcdas09@gmail.com>
2020-04-26 23:48:27 -0400

    Remove duplicates from sorted lsit.

---
+++ problems/remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head == NULL) return NULL;
+        
+        ListNode* root = head;
+        while(head){
+            
+            while(head->next and head->val == head->next->val){
+                head->next = head->next->next;
+            } 
+            head = head->next;
+        }
+        return root; 
+    }
+};
