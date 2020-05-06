cf7551d4b44f7a78b1ee8f7bdc76cdbbbc6e5e85
rajib13 <rcdas09@gmail.com>
2020-05-05 22:15:35 -0400

    Find a tree is symmetric or not.

---
+++ problems/symmetric-tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        
+        stack<TreeNode*> nodes; // To improve the space complexity, we only use one stack. 
+        TreeNode* root1 = root; // For traversing the right side of the tree.
+        
+        while(!nodes.empty() or root != NULL){
+            while(root != NULL){
+                nodes.push(root);
+                if(root1 == NULL) return false; // In a assymmetric tree, right side could be invalid. 
+                nodes.push(root1);
+                
+                root = root->left; // We are traversing root to leftmost node.
+                root1 = root1->right; // another pointer to traverse root to rightmost node.
+            }
+            root = nodes.top(); nodes.pop(); 
+            root1 = nodes.top(); nodes.pop();
+            
+            if(root->val != root1->val) return false; // In a symmetric tree, every two entries of stack should be same. 
+            root = root->right;
+            root1 = root1->left;
+        }
+        
+        return true; 
+    }
+};
