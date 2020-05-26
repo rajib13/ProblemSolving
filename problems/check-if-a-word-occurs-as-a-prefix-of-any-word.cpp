ad7e2aebec133fc74535a7f28a07edf68ff570af
rajib13 <rcdas09@gmail.com>
2020-05-25 23:36:14 -0400

    Find prefix of a pattern in all words of the given sentence.

---
+++ problems/check-if-a-word-occurs-as-a-prefix-of-any-word.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        stringstream ss(sentence);
+        string word;
+        int wordCount = 0;
+        while(ss >> word){
+            wordCount++;
+            if(word.find(searchWord, 0) == 0) return wordCount;
+        }
+        return -1; 
+    }
+};
+
+/*
+Complexity Analysis:
+Time: O(n), as we need to scan all chars (in worst case) of the given sentence.
+Space: O(1), as we do not use any extra space. 
+
+*/
