fe5b980242a7ec1653f539a5e307995f8c552327
rajib13 <rcdas09@gmail.com>
2020-05-28 23:58:01 -0400

    Kids with the greatest nuber of candies.

---
+++ problems/kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int e) {
+        vector<bool> ret;
+        
+        int maxx = 0; 
+        for(int can : candies){
+            maxx = max(maxx, can);
+        }
+        
+        for(int can : candies){
+            if(can + e >= maxx) ret.push_back(true);
+            else ret.push_back(false);
+        }
+        
+        return ret;
+    }
+};
+
+/*
+Complexity analysis:
+Time: O(n), where n is the number of candies.
+Space: O(1), as we did not use any extra space.
+*/
