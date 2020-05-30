9297d7e29b9552f958e667f9b335df816d509153
rajib13 <rcdas09@gmail.com>
2020-05-29 21:52:29 -0400

    Build an array with Push and Pop stack operations.

---
+++ problems/build-an-array-with-stack-options.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> ret;
+        
+        int j = 0;
+        for(int i = 1; i  <= n; i++){
+            if(j == target.size()) break;
+            
+            ret.push_back("Push");
+           
+            if(target[j] == i) j++;
+            else ret.push_back("Pop");
+        }
+        return ret;
+    }
+};
+
+/*
+Complexity Analysis:
+Time: O(n), where n is the given integer.
+Space: O(1), as we did not use any extra space. 
+*/
