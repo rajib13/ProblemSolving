e73c6998fc28b2cd5b3247d4c73daf2240332033
rajib13 <rcdas09@gmail.com>
2020-06-01 23:17:30 -0400

    Invert a binary tree.

--- problems/invert-binary-tree.cpp
+++ problems/invert-binary-tree.cpp
@@ -10,21 +10,6 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        /*Approach 2: Recursive solution*/
-        if(root == nullptr) return root;
-        TreeNode* right = invertTree(root->right);
-        TreeNode* left = invertTree(root->left);
-        root->right = left;
-        root->left = right;
-        return root;
-    }
-};
-
-
-/*
-class Solution {
-public:
-    TreeNode* invertTree(TreeNode* root) {
         // Approach 1: Iterative
         if(root == nullptr) return root;
         TreeNode* head = root;
@@ -43,5 +28,27 @@ public:
         return head;
     }
 };
+
+/*
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        // Approach 2: Recursive solution
+        if(root == nullptr) return root;
+        TreeNode* right = invertTree(root->right);
+        TreeNode* left = invertTree(root->left);
+        root->right = left;
+        root->left = right;
+        return root;
+    }
+};
+
+*/
+
+
+/*
+Complexity Analysis:
+Time: O(n), as we need scan all nodes at least once, where n is the number of nodes of the given tree.
+Space: O(n), as in worst case we the queue ( resp. for the DFS, call stack) needs to hold [n/2] (resp. height of the tree h) nodes. 
 
 */
