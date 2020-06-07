bb56251ae3a2c89558d3c42414b31d50a3e55a00
rajib13 <rcdas09@gmail.com>
2020-06-07 19:26:44 -0400

    Shuffle the elements of an array.

--- problems/shuffle-the-array.cpp
+++ problems/shuffle-the-array.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> ret(2*n, 0);
-        int i = 0; 
-        int j = n;
-        for(int k = 0; k < n; k++){
-            ret[i++] = nums[k];
-            ret[i++] = nums[j++];
-        
+        vector<int> ret;
+        for(int i = 0; i < n; i++){
+            ret.push_back(nums[i]):
+	    ret.push_back(num[i+n]);
         }
         return ret;
     }
 };
+
+/*Complexity Analysis:
+Time : O(n), where n is the number of elements in the given array.
+Space: O(1), since we did not use any extra space.
+*/
