15ee6aec23b250e5fac7320e291c7d6ed1fc682e
rajib13 <rcdas09@gmail.com>
2020-06-09 23:45:28 -0400

    Is subsequence

---
+++ problems/is-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0; 
+        int j = 0;
+        
+        while(j < t.size() and i < s.size()){
+            if(t[j] == s[i]) i++;
+            j++;
+        }
+        return i == s.size();
+    }
+};
+
+/*Complexity Analysis:
+Time: O(n), where n is the lengtth of the t
+Space: O(1), since we do not use any extra space.*/
