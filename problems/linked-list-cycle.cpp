66b6c13b2571a590781e63fcf20205f42f1695f5
rajib13 <rcdas09@gmail.com>
2020-06-18 23:55:46 -0400

    Find a linked list has a cycle or not.

---
+++ problems/linked-list-cycle.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head; // pointer 1
+        ListNode* fast = head; // pointer 2
+        
+        while(fast and fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) return true; // Success case
+        }
+        return false; // Otherwise.
+    }
+};
+
+
+/*Complexity Analysis:
+Time: O(n), as we need to scan at least once.
+Space: O(1), since we do not need any extra space.*/
