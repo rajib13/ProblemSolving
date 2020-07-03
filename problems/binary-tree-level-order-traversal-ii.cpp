9b2959f990a8248ae630dbcac332cca03e1cb4d2
rajib13 <rcdas09@gmail.com>
2020-07-02 22:33:43 -0400

    Binary tree level order traversal II

---
+++ problems/binary-tree-level-order-traversal-ii.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> ret;
+        if(root == nullptr) return ret;
+        queue<TreeNode*> nodes;
+        nodes.push(root);
+        while(!nodes.empty()){
+            int qSize = nodes.size();
+            vector<int> level;
+            for(int i = 0; i < qSize; i++){
+                TreeNode* root = nodes.front(); nodes.pop();
+                level.push_back(root->val);
+                if(root->left) nodes.push(root->left);
+                if(root->right) nodes.push(root->right);
+            }
+            ret.push_back(level);
+        }
+        reverse(ret.begin(), ret.end());
+        return ret;
+    }
+};
+
+
+/* Complexity analysis: 
+Time: O(n), where n is the total number of nodes in the given tree.
+Space: O(n), if it is full binary tree, then at last level there will be n/2 nodes.
+*/
