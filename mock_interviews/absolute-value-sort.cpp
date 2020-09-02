001d393b95d7bf6827687a81be58ad13916555a0
Rajib Das <rajib.das@uleth.ca>
2020-09-01 23:58:19 -0400

    Update absolute-value-sort.cpp

--- mock_interviews/absolute-value-sort.cpp
+++ mock_interviews/absolute-value-sort.cpp
@@ -2,13 +2,14 @@ vector<int> absSort(const vector<int>& arr)
 {
     auto comp = [](int a, int b) { 
         if(abs(a) != abs(b))
-           return abs(a)  < abs(b); // We need absolute so that (-1, 2) works fine! 
+           return abs(a)  < abs(b); // We need absolute value so that (-1, 2) works fine! 
         return a < b; 
     };
     std::sort(arr.begin(), arr.end(), comp);
 }
 
-/*Complexity Analysis: 
+/*
+Complexity Analysis: 
 Time: O(nlogn) as we use the builtin sort with a custom comparison. 
 Space: O(n)
 */ 
