253e54ecdae21f6abfa4f007d2e5254127104251
rajib13 <rcdas09@gmail.com>
2020-09-04 00:01:51 -0400

    Bulb switcher.

---
+++ leetcode/bulb-switcher-iv.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minFlips(string t) {
+      int n = t[0] - '0';
+      for(int i = 1; i< t.size(); i++){
+          if(t[i-1] != t[i]) n++;
+      }
+        return n;
+    }
+};
+
+
+/*
+Complexity analysis:
+Time: O(n), as we need to scan all characters once.
+Space: O(1), since we do not use any extra space.
+*/
