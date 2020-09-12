6d87f07ef707e408b0e40bbb1b974e1b09df6fa7
rajib13 <rcdas09@gmail.com>
2020-09-11 23:22:58 -0400

    K-messed sorted array updated

--- mock_interviews/k-messed-sorted-array.cpp
+++ mock_interviews/k-messed-sorted-array.cpp
@@ -1,24 +1,21 @@
 
-int getMinElement(vector<int>& nums, int i, int k){
-  priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minHeap;
-  for(int j = i; j <= k; j++){
-    minHeap.push(make_pair{nums[j], j});
-  }
-  
-  return minHeap.top().second;
-}
-
 vector<int> sortKMessedArray( const vector<int>& arr, int k ) 
 {
   vector<int> nums = arr;
+  priority_queue<int, vector<int>, greater<int>> minHeap;
+  for(int i = 0; i <= k; i++){
+    minHeap.push(nums[i]);
+  }
   for(int i = 0; i < nums.size(); i++){
-      int j = getMinElement(nums,i, k);
-      swap(nums[i], nums[j]);
+      nums[i] = minHeap.top(); minHeap.pop();
+      if((i + k + 1 ) < arr.size())
+        minHeap.push(nums[i + k + 1]);
   }
   return nums;
 }
 
-/*5. Complexity Analysis:
+/*
+Complexity Analysis:
 Time: O(nlogk), since for every element we are finding the minimum element in the next k elements.
 Space: O(k), since we need to store k elements in the min heap.
 */
