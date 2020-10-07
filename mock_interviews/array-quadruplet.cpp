30e536547d2aebb7553f63a018cb73caa23697be
Rajib Das <rajib.das@uleth.ca>
2020-10-06 22:26:40 -0400

    Update array-quadruplet.cpp

--- mock_interviews/array-quadruplet.cpp
+++ mock_interviews/array-quadruplet.cpp
@@ -3,7 +3,7 @@ vector<int> findArrayQuadruplet(const vector<int> &arr, int s)
 {
   if(arr.size() < 4) return {};
   vector<int> nums = arr;
-  std::sort(nums.begin(), nums.end());
+  sort(nums.begin(), nums.end());
   
   for(int i = 0; i < nums.size()-3; i++){
     for(int j = i + 1; j < nums.size()-2; j++){
@@ -26,6 +26,6 @@ vector<int> findArrayQuadruplet(const vector<int> &arr, int s)
 
 /*
 Complexity analysis:
-Time: O(n^3), where n is the size of the arr.
+Time: O(n^3), where n is the size of the given array.
 Space: O(1), since we did not use any extra space.
 */
