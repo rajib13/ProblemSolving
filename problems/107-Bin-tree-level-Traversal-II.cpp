7547179fd870da80e168522b2b7fba879128317c
Rajib Das <rajib.das@uleth.ca>
2020-11-01 20:36:06 -0500

    Update 107-Bin-tree-level-Traversal-II.cpp

--- problems/107-Bin-tree-level-Traversal-II.cpp
+++ problems/107-Bin-tree-level-Traversal-II.cpp
@@ -43,63 +43,8 @@ public:
         return ret;
     }
 };
-
-
 /*
-        
-        //My solution: 100% space optimized.
-        
-        vector<vector<int>> ret;
-        if(root == NULL) return ret; // Edge Case;
-        
-        vector<int> levelNodes; // To store the level order nodes. 
-
-        queue<TreeNode*> nodes; // Queue for BFS
-        nodes.push(root); // Push root to the queue so that while loop works.
-        
-        int count = 0;
-        int expected  = 1; // expected child for a level, initially 1 because root
-        int nextExp = 0; // next expected child.
-        
-        while(!nodes.empty()){
-            
-            if(expected == count && levelNodes.size() > 0){
-                ret.push_back(levelNodes);
-                levelNodes.clear();
-                expected = nextExp;
-                nextExp = 0;
-                count = 0;
-               
-            } 
-            
-            root = nodes.front(); nodes.pop();
-            levelNodes.push_back(root->val);
-            
-            if(root->left){
-                nodes.push(root->left);
-                nextExp++;
-            } 
-            
-            if(root->right){
-                nodes.push(root->right);
-                nextExp++;
-            } 
-            count++;
-        }
-        if(levelNodes.size() > 0 ) ret.push_back(levelNodes); // For the leaf nodes, the insertion condition (if expected == count...) would not execute,
-        
-        reverse(ret.begin(), ret.end());
-        return ret;
-
-*/
-
-/*
-Test Cases: 
-
-[3,9,20,null,null,15,7]
-[]
-[1,2,3,4,5,6,7,8,9]
-[1,2,3,4,5,6,7,8,9,null,null,8,null,null,6]
-[1,null,2,null,3,4,null,5,null,6,7,null]
-
+    Complexity analysis:
+    Time: O(n), where n is the number of nodes in the given tree. 
+    Space: O(n), in worst case, full binary tree, we need to store n/2 nodes in the queue.
 */
