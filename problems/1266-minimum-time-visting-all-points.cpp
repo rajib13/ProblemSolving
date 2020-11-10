68ac2815f138f7016bf422709608fee74f816712
Rajib Das <rajib.das@uleth.ca>
2020-11-09 23:26:51 -0500

    Update 1266-minimum-time-visting-all-points.cpp

--- problems/1266-minimum-time-visting-all-points.cpp
+++ problems/1266-minimum-time-visting-all-points.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
-        
         int ret = 0; 
         for(int i = 0; i < points.size() - 1; i++){
             ret += max(abs(points[i][0] - points[i+1][0]), abs(points[i][1] - points[i+1][1]));
@@ -11,7 +10,7 @@ public:
 };
    
 /*
-Complexity analysis:
-Time: O(n), as we need to scan all point in the points vector.
-Space: O(1), since we did not any extra space throughout the entirre program.
+    Complexity analysis:
+    Time: O(n), as we need to scan all points once.
+    Space: O(1), since we did not use any extra space throughout the entirre program.
 */
