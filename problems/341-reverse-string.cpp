5923dccea683a9391eec162617ec0a75f13a3bdd
Rajib Das <rajib.das@uleth.ca>
2020-11-13 23:28:13 -0500

    Update 341-reverse-string.cpp

--- problems/341-reverse-string.cpp
+++ problems/341-reverse-string.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int length = s.size(); // determine the size of the given string.
-        if(length > 0){ // crucial because of the empty string.
-            for(int i = 0; i < length/2; i++){ // As we are going to use two pointers from the beginning and the end, it is sufficient to check untill half of the string length.
-                swap(s[i], s[length-i-1]); // length-- is not work, becuase in for loop we determine the limit i every time.  
-            }
-        }
-
+        int i = 0;
+        int j = s.size() - 1;
+        while(i < j)
+            swap(s[i++], s[j--]);
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(n), where n is the length of the given string.
+    Space: O(1), since we did not use any extra space. 
+*/
