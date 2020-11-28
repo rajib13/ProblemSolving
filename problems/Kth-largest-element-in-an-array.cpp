8ea6ef6a7bd921cc0e78f21bb538ae4ce024deee
Rajib Das <rajib.das@uleth.ca>
2020-11-27 23:41:28 -0500

    Update Kth-largest-element-in-an-array.cpp

--- problems/Kth-largest-element-in-an-array.cpp
+++ problems/Kth-largest-element-in-an-array.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> maxHeap;
+        priority_queue<int> minHeap;
         
         for(int i = 0; i < nums.size(); i++){
-            maxHeap.push(nums[i]);
+            minHeap.push(nums[i]);
+            if(minHeap.size() > k) minHeap.pop();
         }
-        
-        for(int i = 0; i < k - 1; i++){
-            maxHeap.pop();
-        }
-        return maxHeap.top();
+        return minHeap.top();
     }
 };
+/*
+    Complexity analysis:
+    Time: O(nlogk), wehre n is the total number of elements in the vector.
+    Space: O(k), since we stored k elements in the min heap.
+*/
