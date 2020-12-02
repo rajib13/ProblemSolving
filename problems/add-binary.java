471cae8c65321d00379276e2aa24df97226f06fa
Rajib Das <rajib.das@uleth.ca>
2020-12-01 23:49:17 -0500

    Update add-binary.java

--- problems/add-binary.java
+++ problems/add-binary.java
@@ -9,7 +9,7 @@ class Solution {
         int carry = 0;
         int sum = 0;
         
-        while(aSize >=0 || bSize >= 0){
+        while(aSize >= 0 || bSize >= 0){
             
             if(aSize >= 0) sum += a.charAt(aSize--) - '0';
             if(bSize >= 0) sum += b.charAt(bSize--) - '0';
@@ -25,3 +25,10 @@ class Solution {
         
     }
 }
+
+/*
+    Complexity analysis:
+    Time: O(max(m,n)), where m and n are the size of string a and b's length, respectively.
+    Space: O(1), since we did not use any extra space.
+
+*/
