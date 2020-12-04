a87d8d80bef9a20e9d56260f71a1c8313947b1d4
Rajib Das <rajib.das@uleth.ca>
2020-12-04 01:11:56 -0500

    Update airplane-seat-assignment-probability.cpp

--- problems/airplane-seat-assignment-probability.cpp
+++ problems/airplane-seat-assignment-probability.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     double nthPersonGetsNthSeat(int n) {
-        return n == 1? 1.0 : 0.5;
-        
+        return n == 1 ? 1.0 : 0.5;
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(1), since the result does not depend on how big the n is!
+    Space: O(1), as we did not use any extra space.
+*/
