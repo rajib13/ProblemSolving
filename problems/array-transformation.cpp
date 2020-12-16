9925139ddc6945354b4b48e40bac2a3de087856a
Rajib Das <rajib.das@uleth.ca>
2020-12-15 23:44:41 -0500

    Update array-transformation.cpp

--- problems/array-transformation.cpp
+++ problems/array-transformation.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     vector<int> transformArray(vector<int>& arr) {
         vector<int> ret = arr;
-    
         while(1){
             int count = 0;
             for(int i = 1; i < arr.size() - 1; i++){
                 if(arr[i] < arr[i-1] and arr[i] < arr[i+1]){ ret[i]++; count++;}
                 else if(arr[i] > arr[i-1] and arr[i] > arr[i+1]) {ret[i]--; count++;}
-               
             }
             if(count == 0) break;
             else arr = ret;
-            
         }
         return ret;
     }
