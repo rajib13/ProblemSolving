b07f86b6ef0154a4e29f8d86dabfe0eef51ae9ea
Rajib Das <rajib.das@uleth.ca>
2020-12-18 23:02:29 -0500

    Update binary-prefix-divisible-by-5.cpp

--- problems/binary-prefix-divisible-by-5.cpp
+++ problems/binary-prefix-divisible-by-5.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     vector<bool> prefixesDivBy5(vector<int>& A) {
-        
         vector<bool> ret;
         int dec = 0;
         for(int i = 0 ; i < A.size(); i++){
             dec = (dec * 2 + A[i]) % 5;
-            cout << dec <<" ";
             if(dec == 0) ret.push_back(true);
             else ret.push_back(false);
         }
         return ret;
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(n), as we need to scan all elements in the given array.
+    Space: O(1), since we did not use any extra space;
+*/
