f4cc83670dbf1f8e9721d1270c735436d1006b42
Rajib Das <rajib.das@uleth.ca>
2020-12-23 22:46:34 -0500

    Update binary-tree-pruning.cpp

--- problems/binary-tree-pruning.cpp
+++ problems/binary-tree-pruning.cpp
@@ -11,13 +11,16 @@ class Solution {
 public:
     TreeNode* pruneTree(TreeNode* root) {
         if(root == NULL) return NULL;
-        
-        root->left = pruneTree(root->left); // recursively call the left sub-tree.
-        root->right = pruneTree(root->right); // recursively call the right sub-tree.
-        
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
         if(root->val == 0 and root->left == NULL and root->right == NULL) return NULL;
         
         return root; 
-        
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(n), where n is the total number of nodes in the given tree, as we need to scan all nodes once.
+    Space: O(h), where h is the height of the tree- the size of the implicit call stack of recursion.
+*/
