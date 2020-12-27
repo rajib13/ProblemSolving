e28972579951ebe7f6eeeedaf6ee2464af222182
Rajib Das <rajib.das@uleth.ca>
2020-12-27 17:08:25 -0500

    Update build-an-array-with-stack-options.cpp

--- problems/build-an-array-with-stack-options.cpp
+++ problems/build-an-array-with-stack-options.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
         vector<string> ret;
-        
         int j = 0;
         for(int i = 1; i  <= n; i++){
             if(j == target.size()) break;
-            
             ret.push_back("Push");
-           
             if(target[j] == i) j++;
             else ret.push_back("Pop");
         }
@@ -17,7 +14,7 @@ public:
 };
 
 /*
-Complexity Analysis:
-Time: O(n), where n is the given integer.
-Space: O(1), as we did not use any extra space. 
+    Complexity Analysis:
+    Time: O(n), where n is the given integer.
+    Space: O(1), as we did not use any extra space. 
 */
