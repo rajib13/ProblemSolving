bfc8a3cf09b94d6dd94baca39c12af32a53fb602
Rajib Das <rajib.das@uleth.ca>
2020-12-31 16:14:04 -0500

    Update climbing-stairs.cpp

--- problems/climbing-stairs.cpp
+++ problems/climbing-stairs.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        
         int first = 0;
         int second = 1;
         
-        for(int i = 2; i<= n; i++){
+        for(int i = 2; i <= n; i++){
             int third = first + second;
             
             first = second;
@@ -13,6 +12,11 @@ public:
             
         }
         return first + second; 
-        
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(n), where n is the number of stairs;
+    Space: O(1), since we did not use any extra space;
+*/
