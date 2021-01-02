573c1fa5ae6114842bb5cc0fe5453cd89637efb9
Rajib Das <rajib.das@uleth.ca>
2021-01-01 19:08:43 -0500

    Update consecutive-chars.cpp

--- problems/consecutive-chars.cpp
+++ problems/consecutive-chars.cpp
@@ -3,10 +3,16 @@ public:
     int maxPower(string s) {
         int maxx = 1;
         int count = 1; 
-        for(int i = 0; i  < s.size()-1; i++){
+        for(int i = 0; i  < s.size() - 1; i++){
             count = s[i] == s[i+1] ?  ++count : 1;
             maxx = max(count, maxx);
         }
         return maxx;
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(n), where 'n' is the length of the given string, as we need to scan every char of the string 's';
+    Space: O(1), since we do not use any extra space;
+*/
