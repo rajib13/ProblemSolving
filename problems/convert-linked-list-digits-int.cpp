1fbb482cb7edc11ffd2b0fc7d1c95d8540fe135d
Rajib Das <rajib.das@uleth.ca>
2021-01-03 18:13:05 -0500

    Update convert-linked-list-digits-int.cpp

--- problems/convert-linked-list-digits-int.cpp
+++ problems/convert-linked-list-digits-int.cpp
@@ -9,13 +9,16 @@
 class Solution {
 public:
     int getDecimalValue(ListNode* head) {
-        
         int ret = 0;
-        
         while(head){
-            ret = ret  * 2 + head->val;
+            ret = ret * 2 + head->val;
             head = head->next;
         }
         return ret; 
     }
 };
+/*
+    Complexity analysis:   
+    Time: O(n), where `n` is the total number of nodes in the given linked list, as we need to scan every node of the list;
+    Space: O(1), since we do not use any extra space throughout the entire program;
+*/
