d6e888dbbe49ce8314db566ae19c7479ffcff0c5
Rajib Das <rajib.das@uleth.ca>
2021-01-25 23:02:12 -0500

    Update defanging-an-ip-address.cpp

--- problems/defanging-an-ip-address.cpp
+++ problems/defanging-an-ip-address.cpp
@@ -5,14 +5,13 @@ public:
         for(int i = 0; i < address.size(); i++){
             if(address[i] == '.') ret += "[.]";
             else ret+= address[i];
-    
-}
+        }
         return ret;
-        
     }
 };
+
 /*
-Complexity analysis:
-Time: O(n), as we need to scan all elements once.
-Space: O(1), since we did not use extra space (the return string is not counted)
+    Complexity analysis:
+    Time: O(n), where `n` is the length of the given string, as we need to scan all elements once.
+    Space: O(1), since we did not use extra space thoroughout the entire program.
 */
