3659770ec6497390d2310765b82f70d1e3ae9576
Rajib Das <rajib.das@uleth.ca>
2021-01-27 23:46:12 -0500

    Update array-transformation.cpp

--- problems/array-transformation.cpp
+++ problems/array-transformation.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> transformArray(vector<int>& arr) {
         vector<int> ret = arr;
+	    
         while(1){
             int count = 0;
             for(int i = 1; i < arr.size() - 1; i++){
@@ -11,9 +12,11 @@ public:
             if(count == 0) break;
             else arr = ret;
         }
+	    
         return ret;
     }
 };
+
 /*
 	Complexity analysis:
 	Time: O(n^2), since we need to traverse the entire array multiple times.
