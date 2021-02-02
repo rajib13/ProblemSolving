0cdce2a3c83dc4ddf31435bf7fba34038fe8d86f
Rajib Das <rajib.das@uleth.ca>
2021-02-01 23:45:10 -0500

    Update convert-sorted-array-to-BST.cpp

--- problems/convert-sorted-array-to-BST.cpp
+++ problems/convert-sorted-array-to-BST.cpp
@@ -10,16 +10,16 @@
 class Solution {
 public:
     TreeNode* sortedArrayToBSTHelper(vector<int>& nums, int start, int end){
-            if(start > end) return nullptr;
-            int mid = (start + end)/2;
-            TreeNode* root = new TreeNode(nums[mid]);
-            root->left = sortedArrayToBSTHelper(nums, start, mid - 1);
-            root->right = sortedArrayToBSTHelper(nums, mid+1, end);
+         if(start > end) return nullptr;
+         int mid = (start + end)/2;
+         TreeNode* root = new TreeNode(nums[mid]);
+         root->left = sortedArrayToBSTHelper(nums, start, mid - 1);
+         root->right = sortedArrayToBSTHelper(nums, mid+1, end);
         
-        return root; 
+         return root; 
     }
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-     return sortedArrayToBSTHelper(nums, 0, nums.size()-1);
+        return sortedArrayToBSTHelper(nums, 0, nums.size()-1);
     }
 };
 
