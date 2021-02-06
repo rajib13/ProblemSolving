3755a2a901c7b5fea03fc6d89b832e74d02c3925
Rajib Das <rajib.das@uleth.ca>
2021-02-05 23:36:59 -0500

    Update delete-tree-nodes.cpp

--- problems/delete-tree-nodes.cpp
+++ problems/delete-tree-nodes.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int deleteTreeNodes(int nodes, vector<int>& parent, vector<int>& value) {
-        
+        int count = 0;
         for(int i = nodes - 1; i > 0; i--){
            value[parent[i]] = value[parent[i]] +  value[i];
-            
         }
-        int count = 0;
+       
         for(int i = 0; i < value.size(); i++){
             if(value[i] == 0){
                 for(int j = 0; j< parent.size(); j++){
@@ -15,11 +14,14 @@ public:
                     }
                 }
             }
-            else{
-                count++;
-            }
+            else count++;
         }
-        
         return count;
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(mn), where `m` is the value and `n` is the parent size. 
+    Space: O(1), since we did not use any auxilary space.
+*/
