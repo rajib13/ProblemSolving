7e692cdafa9fc069a0d590f61dcfd99dc2312277
Rajib Das <rajib.das@uleth.ca>
2021-02-13 23:07:50 -0500

    Update find-numbers-with-even-digit.cpp

--- problems/find-numbers-with-even-digit.cpp
+++ problems/find-numbers-with-even-digit.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int findNumbers(vector<int>& nums) {
-        
         int count = 0; 
         for(int num : nums){
             if(int(log10(num)) % 2 != 0) count++;
@@ -9,3 +8,9 @@ public:
         return count; 
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(logn), where `n` is the length of the given nums array.
+    Space: O(1), since we do not use any extra space.
+*/
