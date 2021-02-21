91ed095b9ddf64524e1b4a2582baef131d74b023
Rajib Das <rajib.das@uleth.ca>
2021-02-20 23:22:36 -0500

    Update fxvii-Merge-Two-Binary-Trees.cpp

--- problems/fxvii-Merge-Two-Binary-Trees.cpp
+++ problems/fxvii-Merge-Two-Binary-Trees.cpp
@@ -10,15 +10,17 @@
 class Solution {
 public: 
     TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
-        
-        if(t1 == NULL) return t2; // if one of them are null then no need to merge, just return it.
+        if(t1 == NULL) return t2; // if one of them are null then no need to merge, just return the other one.
         if(t2 == NULL) return t1;
         t1->val += t2->val;
 
-        t1->left = mergeTrees(t1->left, t2->left); // Do not need to check whether the left or right exists or not because in the next iteration base case will return it. 
+        t1->left = mergeTrees(t1->left, t2->left);
         t1->right = mergeTrees(t1->right, t2->right);
-
         return t1;
-        
     }
 };
+/*
+    Complexity analysis:
+    Time: O(n), where `n` is the length of tree with fewer nodes.
+    Space: O(n), for the recursion stack space.
+*/
