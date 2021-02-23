d5de47a82c794e3cbe479698e8ca3effa403e824
Rajib Das <rajib.das@uleth.ca>
2021-02-22 23:10:01 -0500

    Update h-same-tree.cpp

--- problems/h-same-tree.cpp
+++ problems/h-same-tree.cpp
@@ -11,17 +11,17 @@
  class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        /*Approach 2: recursion. O(n) and O(log n) space - DFS.*/
-        if(p == NULL and q == NULL ) return true; // crucial, because in the next line(s) we try to access the value/childs
-        
+        if(p == NULL and q == NULL ) return true;
         if(p and q == NULL || q and p == NULL || p->val != q->val) return false;
-        
         return isSameTree(p->left, q->left) and isSameTree(p->right, q->right);
-        
     }
 };
  
- 
+/*
+    Complexity analysis:
+    Time: O(n), where `n` is the number of nodes of the give tree.
+    Space: O(n), for the recursion stack space, in worst case (left or right skewed tree) there will be `n` recursion call stored in the call stack.
+*/
 
 /*Approach 1 : O(n) runtime and O(log n) space - BFS level order traversal.
 class Solution {
@@ -53,23 +53,4 @@ public:
         
     }
 };
-
-/*/
-
-
-/*Test Cases
-
-[1,2,3]
-[1,2,3]
-[1]
-[2]
-[1,2,1]
-[1,1,2]
-[1,2,3,4,5,6]
-[1,2,3,4,6,5]
-[1,2,3,4]
-[1,2,3,4,6,5]
-[1,2,3,4,5,6]
-[1,2,3,4]
-
 */
