38308c2546780c370321c96def8e2092d91d205d
Rajib Das <rajib.das@uleth.ca>
2021-02-23 23:56:12 -0500

    Update hhxhii-nim-game.cpp

--- problems/hhxhii-nim-game.cpp
+++ problems/hhxhii-nim-game.cpp
@@ -2,6 +2,11 @@ class Solution {
 public:
     bool canWinNim(int n) {
         return n % 4;
-        
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(1), as the total operations do not depend on the input.
+    Space: O(1), since we do not use any extra space.
+*/
