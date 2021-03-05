605f749035dbc1ff9ccc1e1962bbb3610b173743
Rajib Das <rajib.das@uleth.ca>
2021-03-04 23:09:21 -0500

    Update jewels-and-stones.cpp

--- problems/jewels-and-stones.cpp
+++ problems/jewels-and-stones.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int numJewelsInStones(string J, string S) {
-        
-        
-        vector<char> freq(54,0); // 0-25 is for lowercase and >= 26 is for uppercase letters.
+        vector<char> freq(54, 0); // 0-25 is for lowercase and >= 26 is for uppercase letters.
         
         for(char ch : J){
             if(ch - 'a' < 0) freq[26 + ch-'A']++;
@@ -25,7 +23,7 @@ public:
 };
 
 /*
-Complexity Analysis:
-Time: O(m+n), where m and n are the size of the jewels and stones, respectively.
-Space: O(1), as our freq vector does not depend on the input size.
+    Complexity Analysis:
+    Time: O(m+n), where m and n are the size of the jewels and stones, respectively.
+    Space: O(1), as our freq vector does not depend on the input size.
 */
