2e85023983e8ca259f202d7ea4989e53653d2f11
Rajib Das <rajib.das@uleth.ca>
2021-03-05 23:55:46 -0500

    Update jump-game-III.cpp

--- problems/jump-game-III.cpp
+++ problems/jump-game-III.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    set<int> seen; // To track the visited node so that we do not fall in loop or get TLE. 
-    
+    set<int> seen;
     bool canReach(vector<int>& arr, int start) {
         if(start >= arr.size() or start < 0 or seen.find(start) != seen.end()) return false; // Boundary cases
         seen.insert(start); // populate the seen container
@@ -9,6 +8,10 @@ public:
         if(arr[start] == 0) return true; // success case
         return canReach(arr, start + arr[start]) or canReach(arr, start - arr[start]); // recursive call for two scenarios.
     }
-        
 };
 
+/*
+    Complexity analysis:
+    Time: O(n), at best we we will visit an element twice.
+    Space: O(n), since we need to keep track of the seen values.
+*/
