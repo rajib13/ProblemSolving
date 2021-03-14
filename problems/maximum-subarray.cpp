295c35c65db3de5dedcf8a9e9fbe1a9a114aeb66
Rajib Das <rajib.das@uleth.ca>
2021-03-13 19:59:42 -0500

    Update maximum-subarray.cpp

--- problems/maximum-subarray.cpp
+++ problems/maximum-subarray.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        
-        /*This is the Kadene's algorithm*/
+        /*Approach: Kadene's algorithm*/
         
         int best = nums[0];
         int sum = nums[0];
-        for(int i = 1; i<nums.size(); i++){
+        for(int i = 1; i < nums.size(); i++){
             sum = max(sum+nums[i], nums[i]);
             best = max(best, sum);
-            
         }
         return best; 
-        
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(n), where `n` is the length of the given `nums`, as we need to scan all elements.
+    Space: O(1), since we do not use any extra space.
+*/
