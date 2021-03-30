91544aff360ed436215b843d29473a3458435bed
Rajib Das <rajib.das@uleth.ca>
2021-03-29 22:29:59 -0400

    Update make-two-arrays-equal.cpp

--- problems/make-two-arrays-equal.cpp
+++ problems/make-two-arrays-equal.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& a) {
-        
         sort(target.begin(), target.end());
         sort(a.begin(), a.end());
-        
-        
         
         return target == a; 
     }
 };
+
+/*
+    Complexity analysis:
+    Time:  O(nlogn), where `n` is the length of the given arrays.
+    Space: O(1), since we do not use any extra space.
+*/
