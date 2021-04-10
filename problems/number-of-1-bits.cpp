f9a4ac01d043453c63b58a818bdb8f35f4cb71b4
Rajib Das <rajib.das@uleth.ca>
2021-04-09 22:56:47 -0400

    Update number-of-1-bits.cpp

--- problems/number-of-1-bits.cpp
+++ problems/number-of-1-bits.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        
         int count = 0; 
         while(n){
             count++;
             n &= (n-1);
         }
-        
         return count; 
     }
 };
 
-
 /*
-Complexity Analysis:
-Time: O(1), at the worst case all 32 bits are 1
-Space: O(1), since no additional space is allocated.
-
+    Complexity Analysis:
+    Time: O(1), at the worst case all 32 bits are 1
+    Space: O(1), since no additional space is allocated.
 */
