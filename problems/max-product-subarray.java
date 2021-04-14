3145d44cb4f35d2d9a7989be03661e0b2b0519b3
Rajib Das <rajib.das@uleth.ca>
2021-04-13 23:10:31 -0400

    Update max-product-subarray.java

--- problems/max-product-subarray.java
+++ problems/max-product-subarray.java
@@ -20,3 +20,9 @@ class Solution {
         
     }
 }
+
+/*
+    Complexity analysis:
+    Time: O(n), where `n` is the length of the given `nums`, as we need to scan every element once.
+    Space: O(1), since we do not use any extra space.
+*/
