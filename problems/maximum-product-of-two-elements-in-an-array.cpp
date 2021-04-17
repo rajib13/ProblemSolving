a3513b06a18c891eb341bd510d8df8e6379e78b7
Rajib Das <rajib.das@uleth.ca>
2021-04-16 23:27:44 -0400

    Update maximum-product-of-two-elements-in-an-array.cpp

--- problems/maximum-product-of-two-elements-in-an-array.cpp
+++ problems/maximum-product-of-two-elements-in-an-array.cpp
@@ -16,7 +16,7 @@ public:
 };
 
 /*
-    Complexity Analysis:
-    Time: O(n), as we need to scan all elements once. 
-    Space: O(1), since we do not extra space.
+    Complexity analysis:
+    Time: O(n), where `n` is the  length of the nums, as we need to scan all elements once. 
+    Space: O(1), since we do not use extra space.
 */
