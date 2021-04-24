fc47c6922b377449cb1ce64eb5e82500799424cc
Rajib Das <rajib.das@uleth.ca>
2021-04-23 23:51:27 -0400

    Update number-of-days-between-two-dates.cpp

--- problems/number-of-days-between-two-dates.cpp
+++ problems/number-of-days-between-two-dates.cpp
@@ -1,15 +1,18 @@
 from datetime import date
 
 class Solution:
-    def daysBetweenDates(self, date1: str, date2: str) -> int:
-                   
+    def daysBetweenDates(self, date1: str, date2: str) -> int:    
         year, mon, day = map(int, date1.split('-'))
         year1, mon1, day1 = map(int, date2.split('-'))
         d0 = date(year, mon, day)
         d1 = date(year1, mon1, day1)
         
         delta = abs(d0 - d1)
-        
-        
+
         return delta.days;
-    
+ 
+/*
+    Complexity analysis:
+    Time: O(1), as the the calculation does not depend on the inputs.
+    Space: O(1), since no additional space is allocated.
+*/
