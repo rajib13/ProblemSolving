b6224ff14d19c4477cb6f6a43245b1a2513bd9d2
Rajib Das <rajib.das@uleth.ca>
2021-04-27 23:32:42 -0400

    Update number-sub-arrays-size-k-and-avg-greater-than-equal-threshold.cpp

--- problems/number-sub-arrays-size-k-and-avg-greater-than-equal-threshold.cpp
+++ problems/number-sub-arrays-size-k-and-avg-greater-than-equal-threshold.cpp
@@ -4,20 +4,24 @@ public:
         /* Approach: Sliding window (a and b) algorithm. 
          Although it was not mentioned explicitly, by sub-array it means the continouous subarray. */
         
-        int a = 0;
-        int b = 0;
-        int sum = 0;
-        int count = 0;
+        int a = 0,  b = 0;
+        int sum = 0, count = 0;
         int n = arr.size();
         
-        while(a <= n - k){
-            sum += arr[b++];
-            if(b >= k){
+        while(i <= n - k){
+            sum += arr[j++];
+            if(j >= k){
                 int avg = sum/k;
                 if(avg >= threshold) count++;
-                sum -= arr[a++];
+                sum -= arr[i++];
             }
         }
         return count;
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(n), where `n` is the size of the given arry, as we need to scan every element of the array.
+    Space: O(1), as we do not use any extra space.
+*/
