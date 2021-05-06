2d4d7189d08d99decac9debefe74172bc2e6722a
Rajib Das <rajib.das@uleth.ca>
2021-05-05 23:56:35 -0400

    Update majority-element-ii.cpp

--- problems/majority-element-ii.cpp
+++ problems/majority-element-ii.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         
-        // Try Boyer Moore Voting algorithm from the solution of Majority Element. 
-        
-        /*Idea: if to be a majority element, a number should have to be present more than third of the array size. That means
-         at best two majority elements can be possible. So, we modify the Boyer - Moore algorithm for two possible candidates. 
-         At first step we try to find the majority elements and in the second pass we verify that the elements are present more than
-         n/3 times.
-         
-         
+        /*
+             Idea: to be a majority element, a number should have to be present more than third of the array size. That means
+             at best two majority elements can be possible. So, we modify the Boyer - Moore algorithm for two possible candidates. 
+             At first step we try to find the majority elements and in the second pass we verify that the elements are present more than
+             n/3 times.
          */
         
         if(nums.size() == 0) return {}; // edge case
@@ -21,11 +18,11 @@ public:
         for(int num : nums){
             if(num == candidate1) count1++;
             else if(num == candidate2) count2++;
-            else if(0 == count1){ 
+            else if(count1 == 0){ 
                 candidate1 = num;
                 count1 = 1;
             }
-            else if(0 == count2){ 
+            else if(count2 == 0){ 
                 candidate2 = num;
                 count2 = 1;
             }
@@ -47,7 +44,11 @@ public:
         if(count2 > nums.size()/3) ret.push_back(candidate2);
         
         return ret;
-        
-        
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(n), where `n` is the length of the given array, as we meed to scan every stone at least once.
+    Space: O(n), as we use the priority queue of all stones. 
+*/
