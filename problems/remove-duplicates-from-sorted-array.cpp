3ade19d3454f08260813b09a59a7a8b2fe1d8353
Rajib Das <rajib.das@uleth.ca>
2021-05-08 21:56:46 -0400

    Update remove-duplicates-from-sorted-array.cpp

--- problems/remove-duplicates-from-sorted-array.cpp
+++ problems/remove-duplicates-from-sorted-array.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        /*Approach 2: one liner using unique built in function*/
         nums.erase(unique(nums.begin(), nums.end()), nums.end());
         return nums.size();
     }
 };
 
-
-/* Approach 1: brute force. O(n) time and O(1) space. 
-        if(nums.size() == 0) return 0;
-        
-        int count = 1; 
-        for(int i=1; i< nums.size(); i++){
-            if(nums[i-1] != nums[i]){
-                nums[count++] = nums[i];
-            }
-        }
-        
-     return count; 
+/*
+    Complexity analysis:
+    Time: O(n), where `n` is the length of the given array, as the unique function internaly scans all elements once.
+    Space: O(1), since we do not use any extra space.
 
 */
