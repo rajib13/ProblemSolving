20489875a0e2e4979d005144eb2bc6d6b5f2f978
Rajib Das <rajib.das@uleth.ca>
2021-05-11 23:07:31 -0400

    Update remove-element.cpp

--- problems/remove-element.cpp
+++ problems/remove-element.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        
         int elementCount = 0;
         for(int i = 0; i < nums.size(); i++){
             if(nums[i] != val){
@@ -12,33 +11,8 @@ public:
     }
 };
 
-
-/* Approach 2:  
-
-    The key point here is "It doesn't matter what you leave beyond the new length." 
-    that means you do not need to remove any element but just shift when you find an element
-    equals to given value. 
-    
-    idea: count the element that are equals to val and then shifts non equals value by (i-valCount)
-    and every time you see a non equals element count it as elementCount and finally return elementCount. 
-
-    int removeElement(vector<int>& nums, int val) {
-        
-        int valCount = 0;
-        int elementCount = 0;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] == val){
-                valCount++;
-            }
-            else{
-                if(valCount > 0){
-                    nums[i-valCount] = nums[i];
-                }
-                elementCount++;
-                
-            }
-        }
-        return elementCount; 
-    }
-
-*/
+/*
+    Complexity analysis:
+    Time: O(n), where `n` is the number of elements in the nums, as we need to 
+    Space: O(1), since we do not use any extra space. 
+*/ 
