cc3b0e0b4af8d89a5763ddd646f9f54a5ad8dce0
Rajib Das <rajib.das@uleth.ca>
2021-05-13 21:50:28 -0400

    Update remove_palindromic-subsequences.cpp

--- problems/remove_palindromic-subsequences.cpp
+++ problems/remove_palindromic-subsequences.cpp
@@ -4,7 +4,7 @@ public:
         if(str.size() == 0) return 0;
         // idea: if the given string is already palindrome then we can delete it in 1 step. 
         // If it is not palindrome, and as we are given there will be only two characters, that means
-        // in first iteration, we can delete all 'a's and in second step we aill delete all 'b's, because 
+        // in first iteration, we can delete all 'a's and in second step we will delete all 'b's, because 
         // we need to delete subsequence not the substrings. 
         string temp = str;
         reverse(str.begin(), str.end());
@@ -12,3 +12,9 @@ public:
         
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(n), where `n` is the length of the given `str`, as we need to reverse the string.
+    Space: O(1), since we do not use any extra space.
+*/
