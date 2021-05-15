4f9d54d613cd44f162fb8b203503be3a11ab3e4a
Rajib Das <rajib.das@uleth.ca>
2021-05-14 23:13:42 -0400

    Update reverse-integer.cpp

--- problems/reverse-integer.cpp
+++ problems/reverse-integer.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     int reverse(int x) {
         long rev = 0;
-        /*100% time and 100% space efficients of all submissions.*/
         while(x){
-            int lastDigit = x %10; 
+            int lastDigit = x % 10; 
             x /= 10; 
-            
             rev = rev * 10 + lastDigit;
         }
         
         return rev > INT_MAX || rev < INT_MIN ? 0 : rev;
-        
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(log(x)), as there could be at best log10(x) digits in x.
+    Space: O(1), since we do not use any extra space.
+*/
