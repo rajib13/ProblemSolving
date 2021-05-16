3aed6860d811d6eef7daf4e4b9dfe56bb7ae0528
Rajib Das <rajib.das@uleth.ca>
2021-05-16 16:19:14 -0400

    Update reverse-string.cpp

--- problems/reverse-string.cpp
+++ problems/reverse-string.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        
         int i = 0;
         int j = s.size() - 1;
         
@@ -10,6 +9,8 @@ public:
     }
 };
 
-/*Complexity Analysis:
-Time: O(n), where n is the length of the given string.
-Space: O(1), we reversed the string in place.*/
+/*
+    Complexity analysis:
+    Time: O(n), where `n` is the length of the given string, since we traverse every char of the string from both side. 
+    Space: O(1), as we reversed the string in place.
+*/
