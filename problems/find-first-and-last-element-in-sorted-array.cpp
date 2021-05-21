13697248a12950b8744cd91b6b4a69b3d8bfab00
Rajib Das <rajib.das@uleth.ca>
2021-05-20 23:26:41 -0400

    Update find-first-and-last-element-in-sorted-array.cpp

--- problems/find-first-and-last-element-in-sorted-array.cpp
+++ problems/find-first-and-last-element-in-sorted-array.cpp
@@ -1,26 +1,7 @@
 class Solution {
 public:
-    
-    int binSearch(vector<int>& nums, int target, bool left){
-        int low = 0; 
-        int high = nums.size();
-        
-        while(low < high){
-            int mid = low+(high -low)/2;
-            
-            if(nums[mid] > target || (left && nums[mid] == target)){
-                high = mid;
-            }
-            else{
-                low = mid + 1;
-            }
-        }
-        return low;
-    }
-    
-    /*Approach 2: Binary search*/
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ret={-1, -1};
+        vector<int> ret{-1, -1};
         int startTarget = binSearch(nums, target, true);
         if(startTarget == nums.size() || nums[startTarget] != target){
             return ret;
@@ -31,12 +12,23 @@ public:
         }
         return ret;
     }
-   
+    
+    int binSearch(vector<int>& nums, int target, bool left){
+        int low = 0; 
+        int high = nums.size();
+        
+        while(low < high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] > target || (left && nums[mid] == target)){
+                high = mid;
+            }  else  low = mid + 1;
+        }
+        return low;
+    }
 };
 
 /*
     Complexity analysis:
-    Time: O(logn), where `n` is the number of elements in the nums.
+    Time: O(logn), where `n` is the number of elements in the nums. 
     Space: O(1), since we do not use any extra space. 
-
 */
