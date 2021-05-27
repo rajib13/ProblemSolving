40c7c41a42d535730a3e04bc01934a6335b31d72
Rajib Das <rajib.das@uleth.ca>
2021-05-26 22:03:18 -0400

    Update happy_number.cpp

--- problems/happy_number.cpp
+++ problems/happy_number.cpp
@@ -3,14 +3,14 @@ public:
     int next(int n){
         int m = 0; 
         while(n){
-            m += pow((n % 10),2);
+            m += pow((n % 10), 2);
             n /= 10;
         }
         return m;
     }
     bool isHappy(int n) {
         /*Approach: Floyd Cycle detection algorithm*/
-        int slow = n,fast = next(n); //*** initialize fast one step ahead.
+        int slow = n, fast = next(n); //*** initialize fast one step ahead.
         while(slow != fast){
             slow = next(slow);
             fast = next(fast);
@@ -19,4 +19,10 @@ public:
         return slow == 1;
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(n), as we need to traverse from `n` to 1.
+    Space: O(1), since we do not use any extra space.
+*/
 
