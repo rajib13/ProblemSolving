7fc9225015d53d5cb8ffaa61eec5f45213914231
Rajib Das <rajib.das@uleth.ca>
2021-06-02 23:16:46 -0400

    Update running-sum-1d-array.cpp

--- problems/running-sum-1d-array.cpp
+++ problems/running-sum-1d-array.cpp
@@ -7,3 +7,9 @@ public:
         return nums;
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(n), where `n` is the size of the given nums, as we need to scan all elemlents once.
+    Space: o(1), since we do not use any auxiliary space.
+*/
