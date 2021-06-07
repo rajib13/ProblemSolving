9e9a953adaec0d5b7d5d7506a0faaaf37d7a4b8d
Rajib Das <rajib.das@uleth.ca>
2021-06-06 21:00:05 -0400

    Update search-in-rotated-sorted-array.cpp

--- problems/search-in-rotated-sorted-array.cpp
+++ problems/search-in-rotated-sorted-array.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        
         int low = 0;
         int high = nums.size() - 1; 
         
@@ -24,6 +23,6 @@ public:
 
 /*
     Complexity analysis:
-    Time: O(log n), as at every iteration we removed half of the elements.
-    Space: O(1), since throughout the entire program we did not use any extra space.
+    Time: O(log n), where `n` is the number of elements in the given array, as at every iteration we discard half of the elements.
+    Space: O(1), since throughout the entire program we do not use any extra space.
 */
