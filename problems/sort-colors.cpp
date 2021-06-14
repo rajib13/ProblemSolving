4dc88b08fbea37962b4ee44ead2a01fd90b81cf7
Rajib Das <rajib.das@uleth.ca>
2021-06-13 21:15:34 -0400

    Update sort-colors.cpp

--- problems/sort-colors.cpp
+++ problems/sort-colors.cpp
@@ -1,37 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // Approach: 2 pointer search.
         int redPointer = 0; 
         int bluePointer = nums.size() - 1; 
-        
         
         for(int i = 0; i <= bluePointer; i++){
             if(nums[i] == 0){
                 swap(nums[redPointer++], nums[i]);
             }
             else if(nums[i] == 2){
-                swap(nums[bluePointer--], nums[i--]); // Be careful about i--. Think about it, the answer is nice. See the note. 
+                swap(nums[bluePointer--], nums[i--]);
                 
             }
         }
-        
     }
 };
 
 /*
-
-[2,0,2,1,1,0]
-[0,0,1,1,2,2]
-[0,1,2]
-[2,0,1]
-[0]
-[1]
-[2]
-[0,0,0,0,0]
-[2,2,2,2,0,0,0]
-[1,1,1,1,1,0,0,0,0,2,2,2,2]
-[1,2,0]
-[2,1,2]
-
+    Complexity analysis:
+    Time: O(n), where `n` is the length of the given array, as we need to scan every character of the string once.
+    Space: O(1), since we do not use any additional space. 
 */
