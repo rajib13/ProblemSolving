12c9735abb12f9ccd79cf764212b2d448fa0faa8
Rajib Das <rajib.das@uleth.ca>
2021-06-14 23:15:44 -0400

    Update remove_interval.cpp

--- problems/remove_interval.cpp
+++ problems/remove_interval.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     vector<vector<int>> removeInterval(vector<vector<int>>& in, vector<int>& t) {
-        
         vector<vector<int>>  ret;
-        for(int i = 0; i<in.size(); i++){
+        for(int i = 0; i < in.size(); i++){
             if(in[i][0] < t[0]){
                 ret.push_back({in[i][0], min(in[i][1], t[0])});
                 if(in[i][1] > t[1]){
@@ -29,19 +28,7 @@ public:
 };
 
 /*
-Test Cases:
-[[0,2],[3,4],[5,7]]
-[1,6]
-[[0,5]]
-[2,3]
-[[0,5]]
-[0,0]
-[[0,5],[6,7],[8,10]]
-[5,10]
-[[0,5]]
-[0,5]
-[[8,9]]
-[0,4]
-[[-5,-4],[-3,-2],[1,2],[3,5],[8,9]]
-[-1,4]
+    Complexity analysis:
+    Time: O(n), where `n` is the length of the given intervals, as we need to scan every interval once.
+    Space: O(1), since we do not use any additional space. 
 */
