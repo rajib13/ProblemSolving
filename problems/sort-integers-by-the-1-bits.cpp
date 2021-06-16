0a6aadf1508e72feaaef8e24da321941a36cfe83
Rajib Das <rajib.das@uleth.ca>
2021-06-15 23:57:13 -0400

    Update sort-integers-by-the-1-bits.cpp

--- problems/sort-integers-by-the-1-bits.cpp
+++ problems/sort-integers-by-the-1-bits.cpp
@@ -2,16 +2,22 @@ class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
         vector<int> ret; 
-        multiset<pair<int, int>> s;
+        multiset<pair<int, int>> mset;
         for(int i = 0; i < arr.size(); i++){
             bitset<32> n = bitset<32> (arr[i]);
             int tmp = n.count();
-            s.insert(make_pair(tmp, arr[i]));
+            mset.insert(make_pair(tmp, arr[i]));
         }
-        for(auto elem : s){
+        for(auto elem : mset){
             ret.push_back(elem.second);
         }
         return ret;
         
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(n), where `n` is the length of the given `arr`, as we traverse the entire array once.
+    Space: O(n), since we use the multiset to store the bit count as an additional space. 
+*/
