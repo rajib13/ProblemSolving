69412d491b7ee098ddcdcf4e190032eebca167c9
Rajib Das <rajib.das@uleth.ca>
2021-06-17 22:56:06 -0400

    Update subtract_product_sum.cpp

--- problems/subtract_product_sum.cpp
+++ problems/subtract_product_sum.cpp
@@ -4,15 +4,20 @@ public:
         int product = 1;
         int sum = 0;
         
-        
         while(n > 0){
-            int last = n%10;
-            product *= last;
-            sum += last;
+            int lastDigit = n % 10;
+            product *= lastDigit;
+            sum += lastDigit;
             n /= 10;
-            
         }
         
         return product - sum; 
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(1), as the given number `n` is an integer meaning there could be maximum ten digits, so the loop execute at most ten times.
+    Space: O(1), since we do not use any extra space.
+*/
+
