1f963f0d292aa2964ae09cd7546cda9146ec0888
Rajib Das <rajib.das@uleth.ca>
2021-06-19 22:05:02 -0400

    Update sum-of-left-leaves.cpp

--- problems/sum-of-left-leaves.cpp
+++ problems/sum-of-left-leaves.cpp
@@ -20,33 +20,26 @@ class Solution {
 public:
     int sumOfLeftLeaves(TreeNode* root) {
         if(!root) return 0;
-        
         int ret = 0;
-        
         /***************** Iterative solution *************/
-        
         stack<TreeNode*> nodes;
         
         while(root != NULL || !nodes.empty()){
-            bool flag = false; // to make sure that the root node and the right leaves are not take into account.
+            bool flag = false;
             while(root != NULL){
-                
                 //push left children if available
                 nodes.push(root);
                 if(!root->left and !root->right and flag) ret += root->val;
-                root = root->left; /*** Left ***/
+                root = root->left;
                 flag = true;
             }
-            
-            //retrieve top node and store its right child if exists
             root = nodes.top();
             nodes.pop();
 
-            root = root->right; /*** Right ***/
+            root = root->right;
         }
 
      return ret;
-        
     }
 };
 
@@ -55,13 +48,17 @@ public:
 class Solution {
 public:
     int sumOfLeftLeaves(TreeNode* root, bool isLeftNode = false) {
-        
         // Approach 2: Recursive Solution
         if(!root) return 0;
         if( !root->left and !root->right and isLeftNode ) return root->val;
-        return sumOfLeftLeaves(root->left, true) + sumOfLeftLeaves(root->right, false);
-         
+        return sumOfLeftLeaves(root->left, true) + sumOfLeftLeaves(root->right, false); 
     }
 };
+*/
 
+
+/*
+    Complexity analysis:
+    Time: O(n), where `n` is the number of nodes in the given tree, as we need to iterate through the entire tree once.
+    Space: O(n), in worst case (left or right skewed tree), we need to stroe `n` nodes in the stack. 
 */
