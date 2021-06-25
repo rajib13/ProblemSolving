8a4533af1cdc1d95eb4b5e3d3d30daff02a75f65
Rajib Das <rajib.das@uleth.ca>
2021-06-24 22:39:58 -0400

    Update the-K-weakest-rows-in-a-matrix.cpp

--- problems/the-K-weakest-rows-in-a-matrix.cpp
+++ problems/the-K-weakest-rows-in-a-matrix.cpp
@@ -11,10 +11,13 @@ public:
         for (auto &elem : set){
             if(k-- == 0) break;
             ret.push_back(elem.second);
-
-            
         }
         return ret;
-
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(mn), where `m` and `n` are the number of rows and columns in the given matrix, as we need to iterate through tall of the rows.
+    Space: O(n), since we use a set as an auxiliary space to store the sum of row.
+*/
