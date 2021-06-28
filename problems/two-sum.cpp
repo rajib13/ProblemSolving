5ee3624342b955f8b70799f20390e71613f68600
Rajib Das <rajib.das@uleth.ca>
2021-06-27 22:19:41 -0400

    Update two-sum.cpp

--- problems/two-sum.cpp
+++ problems/two-sum.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-        map<int, int> myMap; //map to store pairs
-        
-        for(int i=0;i<nums.size();i++){
+        unordered_map<int, int> myMap; //map to store pairs
+        for(int i = 0; i < nums.size(); i++){
             int subtract = target - nums[i]; //get the difference between target and the current nums[i] value
             if(myMap.count(subtract)){ // check the difference is in the map or not
-                
                 return {myMap.at(subtract), i}; // find out the index of the difference value
             }
             else{
-                myMap.insert({nums[i],i});
+                myMap.insert({nums[i], i});
             }
         }
         return {};
-        
     }
 };
+
+/*
+    Complexity analysis: 
+    Time: O(n), where `n` is the length of the given array, as we iterate over the entire array once.
+    Space: O(n), since we use a hashmap to store the postion of numbers. 
+*/
