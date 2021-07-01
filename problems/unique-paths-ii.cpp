bf29542013b7ee1b83576bd414bf764affe5034d
Rajib Das <rajib.das@uleth.ca>
2021-06-30 22:46:57 -0400

    Update unique-paths-ii.cpp

--- problems/unique-paths-ii.cpp
+++ problems/unique-paths-ii.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        
         int row = grid.size();
         int col = grid[0].size();
         if(grid[0][0] == 1) return 0; // if start point is an obstacle. 
         
-         
-        vector<vector<long>> dp(row, vector<long> (col, 0)); // Need this extra vector becausee one of the test cases has int overflow.
-        dp[0][0] = 1; // To check the test case [[0]] type. 
+        vector<vector<long>> dp(row, vector<long> (col, 0)); 
+        dp[0][0] = 1; 
         
         // Fill the  first column with 1 until approaching an obstacle and after that with 0. 
         int value = 1;
@@ -31,3 +29,10 @@ public:
         return dp[row-1][col-1];
     }
 };
+
+/*
+    Complexity analysis: 
+    Time: O(m*n), where `m` and `n` are the number of rows and cols in the given grid, respectively, 
+          as we need scan every element of the grid.
+    Space: O(mn), since we use an auxiliary space to to store the intemediate results. 
+*/
