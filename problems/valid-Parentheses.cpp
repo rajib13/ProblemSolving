cc12c30824bdcffd349f7115b7c03ff2454f812c
Rajib Das <rajib.das@uleth.ca>
2021-07-03 00:40:08 -0400

    Update valid-Parentheses.cpp

--- problems/valid-Parentheses.cpp
+++ problems/valid-Parentheses.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool isValid(string s) {
-        
-        stack<char> openBrackets;
-        
+        stack<char> brackets;
         for(char ch : s){
-            switch (ch) {
-                case '(': 
-                case '{': 
-                case '[': openBrackets.push(ch); break;
-                case ')': if(openBrackets.empty() || openBrackets.top() != '(') return false; else openBrackets.pop(); break;
-                case '}': if(openBrackets.empty() || openBrackets.top() != '{') return false; else openBrackets.pop(); break;
-                case ']': if(openBrackets.empty() || openBrackets.top() != '[') return false; else openBrackets.pop(); break;
-            default: ;
-            }
+            if(ch == '(') brackets.push(')');
+            else if(ch == '{') brackets.push('}');
+            else if(ch == '[') brackets.push(']');
+            else if(brackets.empty() or brackets.top() != ch) return false;
+            else brackets.pop();
         }
-        
-        return openBrackets.empty();
-        
+        return brackets.empty();
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(n), where `n` is the length of the given string, as we need to scan every character of the string.
+    Space: O(n), in worst case we need to store all characters in the stack. 
+*/
