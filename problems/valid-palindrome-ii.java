bf24a8f2dc1bddad5f9d3016c4f1e1b04308f1b8
Rajib Das <rajib.das@uleth.ca>
2021-07-04 23:55:09 -0400

    Update valid-palindrome-ii.java

--- problems/valid-palindrome-ii.java
+++ problems/valid-palindrome-ii.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean isPalindrome(String s, int count, int p1, int p2){
-        /*Idea : For any mismatch, we should delete either p1 or p2. As a result we get two substrings and 
-        it is enough to check whether either of these is palindrom or not. MOMENT OF PROUD.*/
-        
         while(p1 < p2){
             if(s.charAt(p1) != s.charAt(p2)){
                 if(count  > 0) return false;
@@ -12,24 +9,17 @@ class Solution {
         }
       return true;
     }
+    
     public boolean validPalindrome(String s) {
         int p1 = 0; 
         int p2 = s.length() - 1;
         
         return isPalindrome(s, 0, p1, p2);
-        
     }
 }
 
 /*
-
-"aba"
-"abda"
-"abcda"
-"abcd"
-"abcdcaa"
-"deeee"
-"dedee"
-"eeeed"
-"eeeeedd"
+    Complexity analysis:
+    Time: O(n), where `n` is the length of the given string, as we need to scan every character of the string.
+    Space: O1n), since we do not use any extra space.
 */
