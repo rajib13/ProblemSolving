438de8ab57841a3aab22ff5719d11d5fc3a0ad96
Rajib Das <rajib.das@uleth.ca>
2021-07-05 23:03:03 -0400

    Update unique-paths.cpp

--- problems/unique-paths.cpp
+++ problems/unique-paths.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     int uniquePaths(int row, int col) {
         vector<int> dp(col, 1);
-        // Approach 2: Just add the previous column value to the current one is enough to get the solution
         for(int r = 1; r < row; r++){
             for(int c = 1; c < col; c++){
                 dp[c] += dp[c-1];
             }
         }
-        
         return dp[col-1];
     }
 };
@@ -16,6 +14,6 @@ public:
 /*
     Complexity analysis: 
     Time: O(m*n), where `m` and `n` are the given number of rows and cols, respectively, 
-          as we need to scan every element in the grid making by row and col.
+          as we need to scan every element in the grid formed by the given row and col.
     Space: O(n), since we use an auxiliary space to store the intemediate results. 
 */
