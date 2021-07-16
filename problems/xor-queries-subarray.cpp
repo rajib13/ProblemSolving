19bd800943e2b7d90c6c3f0c0385a0a70de6b8ef
Rajib Das <rajib.das@uleth.ca>
2021-07-15 21:50:29 -0400

    Update xor-queries-subarray.cpp

--- problems/xor-queries-subarray.cpp
+++ problems/xor-queries-subarray.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        
-        for(int i = 1; i < arr.size(); i++){
+        vector<int> ret;
+        for(int i = 1; i < arr.size(); i++) {
             arr[i] = arr[i-1] ^ arr[i];
         }
-        vector<int> ret;
-        for(int i = 0 ; i < queries.size(); i++){
+        
+        for(int i = 0 ; i < queries.size(); i++) {
             int left = queries[i][0];
             int right = queries[i][1];
             
-            int val = left == 0 ? arr[right] : arr[left-1]^arr[right];
+            int val = left == 0 ? arr[right] : arr[left-1] ^ arr[right];
             ret.push_back(val);
         }
         
         return ret;
     }
 };
+
+/*
+    Complexity analysis:
+    Time: O(m + n), where `m` and `n` are the length of the given `arr` and `queries`, respectively. 
+    Space: O(1), since we do not use any extra space.
+*/
