e0270864473f8137392ae6a9b6ebd76da52c69c9
rajib13 <rcdas09@gmail.com>
2021-07-19 20:15:07 -0400

    Lowest common ancestor of a binary search tree.

---
+++ problems/LcaBst.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while(root != null){
+            if(root.val > p.val && root.val > q.val) root = root.left;
+            else if (root.val < p.val && root.val < q.val) root = root.right;
+            else break;
+        }
+        return root;
+    }
+}
+
+/*Complexity analysis:
+  Time: O(logn), where `n` is the number of nodes in the given BST, as we discard half of the nodes at every step.
+  Space: O(1), since we do not use any extra space.
+*/
