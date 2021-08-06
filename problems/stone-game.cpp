032c939874ac75711d430794d02955d843450154
rajib13 <rcdas09@gmail.com>
2021-08-05 23:58:28 -0400

    Stone game added

---
+++ problems/stone-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+       return true; 
+    }
+};
+
+/*
+   Complexity analysis:
+   Time: O(1), as the output is always same or does not depend on the input size.
+   Space: O(1), since we we do not use an extra space.
+*/
