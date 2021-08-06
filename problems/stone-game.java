0dae80c8e181aa720ecf6acdffd0673a63dc0023
rajib13 <rcdas09@gmail.com>
2021-08-06 19:26:44 -0400

    Stone game added

---
+++ problems/stone-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
+
+/*
+   Complexity analysis:
+   Time: O(1), as the output is always same or does not depend on the input size.
+   Space: O(1), since we we do not use an extra space.
+*/
