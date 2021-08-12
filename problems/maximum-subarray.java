985bb7352bb3d518ffe318d5ec77f1ce38c197f9
rajib13 <rcdas09@gmail.com>
2021-08-12 00:03:14 -0400

    Maximum subarry

--- problems/maximum-subarray.java
+++ problems/maximum-subarray.java
@@ -1,11 +1,17 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int best = 0;
+        int best = Integer.MIN_VALUE;
         int maxx = 0;
-        for(int num : nums){
-            maxx = Math.max(maxx + num, num);
+        for(int i = 0; i < nums.length; i++){
+            maxx = Math.max(maxx + nums[i], nums[i]);
             best = Math.max(best, maxx);
         }
         return best;
     }
 }
+
+/*
+   Complexity analysis:
+   Time: O(n), where `n` is the length of the given nums, as we need to scan all elements of the array.
+   Space: O(1), since we do not use any extra space.
+*/
